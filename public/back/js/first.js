103f60e8353de89bb04aa53adb257184265bb93c
gitliuyaxuan <491407961@qq.com>
2018-11-30 11:07:51 +0800

    一级分类添加完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -39,4 +39,55 @@ function render() {
   })
 }
 
+// 2. 点击添加按钮 显示添加模态框
+$('#addBtn').click(function(){
+  $('#addModal').modal('show'); //显示
+});
+
+// 3. 进行表单校验
+$('#form').bootstrapValidator({
+
+  // 配置校验图标
+  feedbackIcons: {
+    valid: 'glyphicon glyphicon-ok',    // 校验成功
+    invalid: 'glyphicon glyphicon-remove',   // 校验失败
+    validating: 'glyphicon glyphicon-refresh'  // 校验中
+  },
+  //校验字段
+  fields: {    // input框中需要配置 name
+    categoryName: {
+      validators: {
+        notEmpty: {
+          message: "请输入一级分类名称"
+        }
+      }
+    }
+  }
+});
+
+// 4. 注册表单校验成功事件 阻止默认的提交 通过ajax提交
+$('#form').on('success.form.bv',function(e){
+  e.preventDefault(); // 阻止默认的提交
+  // 通过ajax提交
+  $.ajax({
+    type: 'post',
+    url: '/category/addTopCategory',
+    data: $('#form').serialize(),
+    dataType: 'json',
+    success:function(info){
+      console.log(info);
+      if (info.success) {
+        // 关闭模态框
+        $('#addModal').modal('hide');
+        //重新渲染页面 渲染第一页
+        currentPage = 1;
+        render();
+
+        //重置表单的内容和状态
+        $('#form').data('bootstrapValidator').resetForm(true)
+      }
+    }
+  })
+})
+
 })
